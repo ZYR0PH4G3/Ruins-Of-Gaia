79c946dbbdc9d59648ccc45b95a7d8933465b9be
ZYR0PH4G3 <shukla.d3stry0.arnav@gmail.com>
2026-07-28 16:45:50 +0530

    made a little progress with exit from cliffside scene

--- ruins-of-gaia/scenes/cliff_side.gd
+++ ruins-of-gaia/scenes/cliff_side.gd
@@ -12,8 +12,10 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.has_method("player"):
+		Global.transition_scene = true
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.has_method("player"):
+		Global.transition_scene = false
